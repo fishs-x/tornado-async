68583f9a50f87e9b50dd33f12ee544b5896ca8cf
zhangzhch <zhangzhch2016@gmail.com>
2019-04-12 08:38:42 +0800

    Delete t2.py

--- t2.py
+++
@@ -1,9 +0,0 @@
-from models.users import Users
-from databases import MysqlPool
-
-db = MysqlPool().get_conn
-
-
-with db.atomic() as transaction:
-    Users.create(name='123xxx')
-    Users.create(name='234xxx')
