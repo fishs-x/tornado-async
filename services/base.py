ce555bb7482ec9c2faff62259cc59f2ee14d02c2
zhangzhch <zhangzhch2016@gmail.com>
2019-04-30 16:08:29 +0800

    Update base.py

--- services/base.py
+++ services/base.py
@@ -3,7 +3,6 @@ from loguru import logger
 from peewee_async import execute
 from databases import MysqlPool, RedisPool
 
-
 class BaseService:
     model: Model = None
     _service = dict()
@@ -21,19 +20,61 @@ class BaseService:
 
     @staticmethod
     async def execute(sql):
-        return await execute(sql)
+        """执行sql"""
+        try:
+            return await execute(sql)
+        except Exception as e:
+            logger.exception(str(e))
+            return None
 
     async def insert(self, **kwargs):
-        return await self.db.create(self.model, **kwargs)
-
-    async def find_one(self, *args, **kwargs):
+        """保存数据"""
         try:
-            return await self.db.get(self.model, *args, **kwargs)
+            return await self.db.create(self.model, **kwargs)
+        except Exception as e:
+            logger.exception(str(e))
+            return None
+
+    async def update(self, data):
+        """更新一条数据"""
+        try:
+            return await self.db.update(data)
         except self.model.DoesNotExist:
             return None
         except Exception as e:
             logger.exception(str(e))
             return None
+
+    async def find_one(self, *args, **kwargs):
+        """查找一条数据"""
+        try:
+            if not args:
+                return await self.db.get(self.model, *args, **kwargs)
+            res = await self.find(*args, **kwargs)
+            if len(res) > 1:
+                raise Exception("find multiple data")
+            elif len(res) == 1:
+                return res[0]
+            return None
+        except self.model.DoesNotExist:
+            return None
+        except Exception as e:
+            logger.exception(str(e))
+            return None
+
+    async def find(self, *args, **kwargs):
+        """查询指定字段，查找多条数据"""
+        if not args:
+            raise Exception("fields is empty")
+
+        sql = self.model.select(*[getattr(self.model, k) for k in args])
+        for key, val in kwargs.items():
+            sql = sql.where(getattr(self.model, key) == val)
+        try:
+            return await self.db.execute(sql)
+        except Exception as e:
+            logger.exception(str(e))
+            return []
 
     @property
     def db(self):
